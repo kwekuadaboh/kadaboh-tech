4443d0d2e0a20f1e60bdfd35fb51fb98c79204be
kwekuadaboh <kwekuadaboh3@gmail.com>
2020-11-19 00:31:25 -0500

    first commit

---
+++ src/scripts/app.js
@@ -0,0 +1,9 @@
+import "../sass/main.scss";
+import "../scripts/modules/siteNav.js";
+import "../scripts/modules/projects.js";
+// import "../scripts/modules/projects.js
+
+if (module.hot) {
+  // Enables HMR for js
+  module.hot.accept();
+}
---
+++ src/scripts/vendor.js
@@ -0,0 +1,4 @@
+import "../vendorCss/normalize.css";
+import "../vendorCss/skeleton.css";
+
+// console.log("aWelcome from Vendor ");
