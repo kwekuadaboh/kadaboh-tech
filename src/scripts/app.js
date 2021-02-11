3b9c7294fb04fe14447a24cc504de9ed5fec5df2
kwekuadaboh <kwekuadaboh3@gmail.com>
2021-02-11 10:49:09 -0500

    untracked src

--- src/scripts/app.js
+++
@@ -1,9 +0,0 @@
-import "../sass/main.scss";
-
-import "../scripts/modules/project.js";
-// import "../scripts/modules/projects.js
-
-if (module.hot) {
-  // Enables HMR for js
-  module.hot.accept();
-}
--- src/scripts/vendor.js
+++
@@ -1,4 +0,0 @@
-import "../vendorCss/normalize.css";
-import "../vendorCss/skeleton.css";
-
-// console.log("aWelcome from Vendor ");
